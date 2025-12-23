80a795a4e770646666e93148ac838cea9610fca2
yard <yeoun@ironvalley.co.kr>
2025-12-23 16:45:52 +0900

    Feat: Add tools.py
    firecrawl을 사용한 웹 검색, firecrawl api key 필요
    검색 결과에서 불필요한 부분 정규식으로 제거, token 절약을 위함

---
+++ Job_Hunter_Agent/tools.py
@@ -0,0 +1,41 @@
+import os, re
+
+from crewai.tools import tool
+from firecrawl import FirecrawlApp, ScrapeOptions
+
+
+@tool
+def web_search_tool(query: str):
+    app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+
+    response = app.search(
+        query=query,
+        limit=5,
+        scrape_options=ScrapeOptions(
+            formats=["markdown"],
+        ),
+    )
+
+    if not response.success:
+        return "Error using tool."
+
+    cleaned_chunks = []
+
+    for result in response.data:
+
+        title = result["title"]
+        url = result["url"]
+        markdown = result["markdown"]
+
+        cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
+        cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+
+        cleaned_result = {
+            "title": title,
+            "url": url,
+            "markdown": cleaned,
+        }
+
+        cleaned_chunks.append(cleaned_result)
+
+    return cleaned_chunks
